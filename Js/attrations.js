8c495be322da2ceb7eeccee38b472ef27686769a
keegankelley78-dot <keegankelley78@gmail.com>
2026-05-20 09:33:43 -0400

    5/20

--- Js/attrations.js
+++ Js/attrations.js
@@ -1,11 +1,32 @@
+const attrationsContainer = document.getElementById("attrationsContainer");
+
+async function loadAttrations() {
+    try {
+        const response = await fetch("/Res/Data/attrations.json");
+
+        if (!response.ok) {
+            throw new Error("Could not load attrations JSON file.");
+        }
+
+        const attrations = await response.json();
+
+        createAttrationCards(attrations);
+
+    } catch (error) {
+        console.error("Attrations loading error:", error);
+
+        attrationsContainer.innerHTML = `
+            <p class="error_message">Could not load attractions.</p>
+        `;
+    }
+}
+
 function createAttrationCards(attrations) {
     attrationsContainer.innerHTML = "";
 
     attrations.forEach(attration => {
         const card = document.createElement("div");
-
         card.classList.add("attrations_card");
-        card.id = attration.id;
 
         card.innerHTML = `
             <div class="attrations_top">
@@ -32,3 +53,40 @@ function createAttrationCards(attrations) {
 
     setupAttrationButtons();
 }
+
+function createPlaces(places) {
+    return places.map(place => {
+        return `
+            <div class="place_card">
+                <img src="${place.image}" alt="${place.alt}">
+
+                <div class="place_text">
+                    <h3>${place.name}</h3>
+                    <p>${place.description}</p>
+                </div>
+            </div>
+        `;
+    }).join("");
+}
+
+function setupAttrationButtons() {
+    const toggles = document.querySelectorAll(".toggle");
+
+    toggles.forEach(toggle => {
+        toggle.addEventListener("click", e => {
+            const card = e.target.closest(".attrations_card");
+
+            if (card) {
+                card.classList.toggle("active");
+
+                if (card.classList.contains("active")) {
+                    toggle.textContent = "Show Less";
+                } else {
+                    toggle.textContent = "More Info";
+                }
+            }
+        });
+    });
+}
+
+loadAttrations();
